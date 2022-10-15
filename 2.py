566ac231218806382280ebb7da5d80fc09e085b3
sers <sergeimeizu04@gmail.com>
2022-10-15 16:46:28 +0300

    student

--- 2.py
+++ 2.py
@@ -1,1 +1,11 @@
-print("Heloo World")
+class student:
+    print("Hi")
+    def  __init__(self, height=160):
+        self.height = height
+
+
+vasya  = student()
+print(vasya.height)
+
+dima = student(height=186 )
+print(dima.height)
