8f232be65f351cee0176400514202ab9e61e4b08
sers <sergeimeizu04@gmail.com>
2022-10-21 13:05:30 +0300

    dz

--- student.py
+++ student.py
@@ -4,19 +4,22 @@ class Student:
         self.name = name
         self.gladness = 50
         self.progress = 0
-        self.money
+        self.maney = 500
         self.alive = True
     def to_study(self):
         print("Time to study")
-        self.progress += 0.12
-        self.gladness -= 5
+        self.progress += 0.15
+        self.gladness -= 8
+        self.maney += 5
     def to_sleep(self):
         print("I will sleep")
-        self.gladness += 3
+        self.gladness += 5
+        self.maney -= 4
     def to_chill(self):
         print("Rest time")
         self.gladness += 5
         self.progress -= 0.1
+        self.maney -= 5
     def is_alive(self):
         if self.progress < -0.5:
             print("Cast out…")
@@ -27,9 +30,14 @@ class Student:
         elif self.progress > 5:
             print("Passed externally…")
             self.alive = False
+        elif  self.maney <= 0:
+            print("died of hunger")
+            self.alive = False
+
     def end_of_day(self):
         print(f"Gladness = {self.gladness}")
         print(f"Progress ={round(self.progress, 2)}")
+        print(f"Maney  = {self.maney}")
 
     def live(self, day):
         day = "Day" + str(day) + "of" + self.name + "life"
