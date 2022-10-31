0d4d665c78d9a44910a06223fb288281c6dd773f
sers <sergeimeizu04@gmail.com>
2022-10-31 14:11:06 +0300

    dz1

--- 123.py
+++ 123.py
@@ -2,29 +2,29 @@ import random
 class Student:
     def __init__(self, name: object) -> object:
         self.name = name
-        self.gladness = 50
+        self.gladness = 10
         self.progress = 0
-        self.maney = 300
+        self.maney = 400
         self.alive = True
     def to_study(self):
         print("Time to study")
-        self.progress += 1
-        self.gladness -= 9
+        self.progress += 0.15
+        self.gladness -= 6
         self.maney += 5
     def to_sleep(self):
         print("I will sleep")
-        self.gladness += 10
-        self.maney -= 5
+        self.gladness += 6
+        self.maney -= 4
     def to_chill(self):
         print("Rest time")
-        self.gladness += 5
-        self.progress -= 0.15
+        self.gladness += 6
+        self.progress -= 0.01
         self.maney -= 5
     def to_job(self):
         print("Time to job")
-        self.maney += 5
-        self.progress +=0.50
-        self.gladness -=0.60
+        self.maney += 1
+        self.progress +=0.05
+        self.gladness -=0.09
 
     def is_alive(self):
         if self.progress < -0.5:
@@ -33,10 +33,10 @@ class Student:
         elif self.gladness <= 0:
             print("Depression…")
             self.alive = False
-        elif self.progress > 5:
+        elif self.progress >= 19:
             print("Passed externally…")
             self.alive = False
-        elif self.maney <= 0:
+        elif  self.maney <= 0:
             print("died of hunger")
             self.alive = False
 
@@ -44,7 +44,7 @@ class Student:
     def end_of_day(self) -> object:
 
         print(f"Gladness = {self.gladness}")
-        print(f"Progress = {round(self.progress, 2)}")
+        print(f"Progress ={round(self.progress, 2)}")
         print(f"Maney  = {self.maney}")
 
     def live(self, day):
@@ -53,12 +53,12 @@ class Student:
         live_cube = random.randint(1, 4)
         if live_cube == 1:
             self.to_study()
+        elif live_cube == 2:
+            self.to_sleep()
         elif live_cube == 3:
             self.to_chill()
         elif live_cube == 4:
             self.to_job()
-        elif live_cube == 2:
-            self.to_sleep()
         self.end_of_day()
         self.is_alive()
 
